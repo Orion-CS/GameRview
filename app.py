3ca5634ced6bec79b37164e86f60ec4a27545e77
Orion202021 <94141243+Orion202021@users.noreply.github.com>
2022-10-25 13:01:52 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# SpiderWeb Semester Project
+
+from flask import Flask, request, render_template, redirect, url_for, abort
+from flask import flash
+
+# make sure the script's directory is in Python's import path
+# this is only required when run from a different directory
+import os, sys
+script_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(script_dir)
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'spiderweb'
+
+@app.route('/')
+def index():
+    pass
