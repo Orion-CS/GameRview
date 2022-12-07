3682ef89810844d658fb7d73ccfecc9415c2eab5
gacharyzoniea <zgoniea@gmail.com>
2022-12-07 11:51:46 -0500

    Login formatting

--- static/scripts/rating.js
+++ static/scripts/rating.js
@@ -49,8 +49,8 @@ function fixStars(ratingVal){
     if(responseField.value != ratingVal){
             active = ratingVal;
             fill(active);
-        }
     }
+}
 
 function onStarOut(){
     fill(active);
@@ -62,38 +62,3 @@ function onStarClick(i){
     responseField.value = i+1;
     fill(active);
 }
-//     const starButton1 = document.getElementById("star1");
-//     const starButton2 = document.getElementById("star2");
-//     const starButton3 = document.getElementById("star3");
-//     const starButton4 = document.getElementById("star4");
-//     const starButton5 = document.getElementById("star5");
-//     starButton1.addEventListener("click", fill_in1);
-//     starButton2.addEventListener("click", fill_in2);
-//     starButton3.addEventListener("click", fill_in3);
-//     starButton4.addEventListener("click", fill_in4);
-//     starButton5.addEventListener("click", fill_in5);
-
-// });
-
-// function fill_in1() {
-//     starButton1.style.color = "yellow";
-// }
-
-// function fill_in2() {
-//     starButton2.style.color = "yellow";
-// }
-
-// function fill_in3() {
-//     starButton3.style.color = "yellow";
-// }
-
-// function fill_in4() {
-//     starButton4.style.color = "yellow";
-// }
-
-// function fill_in5() {
-//     starButton5.style.color = "yellow";
-// }
-
-// const stars = document.querySelectorAll(".stars a");
-// const star = document.querySelector(".stars");
