01af6a2815a7fcf2eb9127a88970d82c0f27a5e4
Orion202021 <94141243+Orion202021@users.noreply.github.com>
2022-12-09 15:19:07 -0500

    ajax search

--- static/scripts/gamepage.js
+++ static/scripts/gamepage.js
@@ -20,5 +20,81 @@ window.addEventListener("DOMContentLoaded", async () => {
             reviews[j].querySelector('#stars').appendChild(starImg);
         }       
     };
+
+    const favoriteButton = document.getElementById("favorite-button");
+    favoriteButton.addEventListener("click", toggleFavorite);
+
+    setupFavorite();
 });
 
+function toggleFavorite() {
+    const gameIdText = document.getElementById("game-id");
+    const gameId = gameIdText.innerText
+
+    const userIdText = document.getElementById("user-id");
+    const userId = userIdText.innerText
+
+    var send_data = [
+        {"gameId": gameId},
+        {"userId": userId}
+      ];
+
+    console.log("HERE 1")
+
+    $.ajax({
+        type: "POST",
+        url: "/toggleFavorite/",
+        data: JSON.stringify(send_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result) {
+            toggle(result)
+          } 
+    });
+}
+
+function toggle(result) {
+    const favoriteButton = document.getElementById("favorite-button");
+
+    // check if is favorite
+    if (result.favorite) {
+        favoriteButton.innerText = "Un-Favorite"
+    } else {
+        favoriteButton.innerText = "Favorite"
+    }
+}
+
+function setupFavorite() {
+    const gameIdText = document.getElementById("game-id");
+    const gameId = gameIdText.innerText
+
+    const userIdText = document.getElementById("user-id");
+    const userId = userIdText.innerText
+
+    var send_data = [
+        {"gameId": gameId},
+        {"userId": userId}
+      ];
+
+    $.ajax({
+        type: "POST",
+        url: "/getFavorite/",
+        data: JSON.stringify(send_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result) {
+            setFavorite(result)
+          } 
+    });
+}
+
+function setFavorite(result) {
+    const favoriteButton = document.getElementById("favorite-button");
+
+    // check if is favorite
+    if (result.favorite) {
+        favoriteButton.innerText = "Un-Favorite"
+    } else {
+        favoriteButton.innerText = "Favorite"
+    }
+}
