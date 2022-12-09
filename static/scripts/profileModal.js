f5caeae018fed7a73d2814cdb9519f99d6acb887
amycunninghamm <cunninghamae19@gcc.edu>
2022-12-09 11:46:46 -0500

    user edit info stuff it dosent work yet tho

--- static/scripts/profileModal.js
+++ static/scripts/profileModal.js
@@ -2,12 +2,12 @@ var profileModal = document.getElementById("profileModal");
 
 var modalBtn = document.getElementById("profileBtn");
 
-var closeSpan = document.getElementsByClassName("close")[0];
+var closeBtn = document.getElementsByClassName("close")[0];
 
 modalBtn.onclick = function() {
     profileModal.style.display = "block";
 }
 
-closeSpan.onclick = function() {
+closeBtn.onclick = function() {
     profileModal.style.display = "none";
 }
